23c7f009f4528d5d5ceb7039a708d4f1a71f3107
Munavvarhushen Durvesh <munavvard222@gmail.com>
2024-08-08 13:30:28 +0530

    maryUI with login/register and simple crud of users

---
+++ resources/views/livewire/login.blade.php
@@ -0,0 +1,55 @@
+<?php
+
+use Livewire\Attributes\Layout;
+use Livewire\Attributes\Rule;
+use Livewire\Attributes\Title;
+use Livewire\Volt\Component;
+
+new
+#[Layout('components.layouts.empty')]
+#[Title('Login')]
+class extends Component {
+
+    #[Rule('required|email')]
+    public string $email = '';
+
+    #[Rule('required')]
+    public string $password = '';
+
+    public function mount()
+    {
+        if (auth()->user()) {
+            return redirect('/');
+        }
+    }
+
+    public function login()
+    {
+        $credentials = $this->validate();
+
+        if (auth()->attempt($credentials)) {
+            request()->session()->regenerate();
+
+            return redirect()->intended('/');
+        }
+
+        $this->addError('email', 'Please Check USERNAME and PASSWORD.');
+    }
+}
+
+?>
+
+
+<div class="md:w-96 mx-auto mt-20">
+    <div class="mb-10">Cool image here</div>
+
+    <x-form wire:submit="login">
+        <x-input label="E-mail" wire:model="email" icon="o-envelope" inline />
+        <x-input label="Password" wire:model="password" type="password" icon="o-key" inline />
+
+        <x-slot:actions>
+            <x-button label="Create an account" class="btn-ghost" link="/register" />
+            <x-button label="Login" type="submit" icon="o-paper-airplane" class="btn-primary" spinner="login" />
+        </x-slot:actions>
+    </x-form>
+</div>
--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,25 @@
 <?php
 
 use Livewire\Volt\Volt;
+use Illuminate\Support\Facades\Route;
 
-Volt::route('/',"index");
-Volt::route('/users', 'users.index');
-Volt::route('/users/create', 'users.create');
-Volt::route('/users/{user}/edit', 'users.edit');
+// Users will be redirected to this route if not logged in
+Volt::route('/login', 'login')->name('login');
+Volt::route('/register', 'register')->name('register');
 
+// Define the logout
+Route::get('/logout', function () {
+    auth()->logout();
+    request()->session()->invalidate();
+    request()->session()->regenerateToken();
+    return redirect('/');
+});
+
+// Protected routes here
+Route::middleware('auth')->group(function () {
+    Volt::route('/', 'index');
+    Volt::route('/users', 'users.index');
+    Volt::route('/users/create', 'users.create');
+    Volt::route('/users/{user}/edit', 'users.edit');
+    // ... more
+});
